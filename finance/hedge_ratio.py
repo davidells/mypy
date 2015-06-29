bc43e66f19c9debad5f6614a516a394a19a9addc
David Ells <ells.david@gmail.com>
2015-06-28 22:36:30 -0500

    Add hedge_ratio with tests

---
+++ finance/hedge_ratio.py
@@ -0,0 +1,31 @@
+import scipy.odr as odr
+
+from sklearn.linear_model import LinearRegression
+
+# Concepts adapted from http://quanttrader.info/public/betterHedgeRatios.pdf
+def hedge_ratio(x, y, method="ols"):
+    
+    if method == "ols":
+        model = LinearRegression().fit(x[:,None], y)
+        return model.coef_
+
+    elif method == "tls":
+        return odr.ODR(
+            odr.Data(x, y), 
+            odr.Model(lambda B,x: B[0]*x + B[1]),
+            beta0=[0,0]).run().beta[0]
+    else:
+        raise ValueError("method must equal 'ols' (ordinary least squares) "
+                         " or 'tls' (total least squares)")
+
+# TODO: 
+# 
+# hedgeRatios <- function(x, y, lookback = 20, ...) {
+#   df <- as.xts(data.frame(x, y))
+#   rollapply(df,
+#     width = lookback, 
+#     by.column = FALSE,
+#     FUN = function (window) { 
+#       hedgeRatio(window[,1], window[,2], ...) 
+#   })
+# }
---
+++ finance/hedge_ratio_test.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import pandas.io.data as web
+import scipy.odr as odr
+
+from datetime import datetime
+from hedge_ratio import hedge_ratio
+from sklearn.linear_model import LinearRegression
+
+from nose.tools import assert_almost_equals, raises
+
+def setup_module():
+    global EWA, EWC
+    EWA = pd.read_csv("EWA.csv")['Adj Close']
+    EWC = pd.read_csv("EWC.csv")['Adj Close']
+    
+def test_hedge_ratio_ols():
+    model = LinearRegression().fit(EWA[:,None], EWC)
+    assert_almost_equals(
+        model.coef_, 
+        hedge_ratio(EWA, EWC, method="ols"),
+        places=10)
+    
+def test_hedge_ratio_tls():
+    model = odr.ODR(
+        odr.Data(EWA, EWC),
+        odr.Model(lambda B,x: B[0]*x + B[1]),
+        beta0=[0,0]).run()
+
+    assert_almost_equals(
+        model.beta[0], 
+        hedge_ratio(EWA, EWC, method="tls"),
+        places=10)
+
+@raises(ValueError)
+def test_hedge_ratio_bad_arg():
+    hedge_ratio(EWA, EWC, method="unknown")
+    
+if __name__=='__main__':
+    setup_module()
+    test_hedge_ratio_ols()
+    test_hedge_ratio_tls()
